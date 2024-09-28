98a2660bb15ff8bc7f44a81ca1a60f736ef890e4
Varnan Rathod <varnanrathod6@gmail.com>
2024-09-28 08:09:49 +0530

    main js updated.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './styles/App.css';
 import Header from "./components/header.jsx";
+import LandingPage from './components/Landing.jsx';
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <LandingPage></LandingPage>
     </div>
   );
 }
